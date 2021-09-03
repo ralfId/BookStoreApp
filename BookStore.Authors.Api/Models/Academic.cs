8e7f3d329da82633e64726927c4174d8ad0e7dca
rafaelId <ralf_raid@outlook.es>
2021-09-02 21:46:46 -0600

    first commit, add solution and Authors.Api proyect with models Authors and Academic

---
+++ BookStore.Authors.Api/Models/Academic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Authors.Api.Models
+{
+    public class Academic
+    {
+        public int AcademicId { get; set; }
+        public string AcademicGuid { get; set; }
+        public string Name { get; set; }
+        public string Institution { get; set; }
+        public DateTime? DateGraduation { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
---
+++ BookStore.Authors.Api/Models/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Authors.Api.Models
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string AuthorGuid { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public DateTime? Birthday { get; set; }
+
+        public ICollection<Academic> AcademicDegreeList { get; set; }
+    }
+}
