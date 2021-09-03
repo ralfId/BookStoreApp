43e7a93e11e0965ad7a88a21bb12370a94161691
rafaelId <ralf_raid@outlook.es>
2021-09-03 16:15:25 -0600

    Create Books.Api, Add models and db context

---
+++ BookStore.Books.Api/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Books.Api.Models
+{
+    public class Book
+    {
+        public Guid BookId { get; set; }
+        public string Title { get; set; }
+        public DateTime? PublicationDate { get; set; }
+        public Guid AuthoBook { get; set; }
+    }
+}
---
+++ BookStore.Books.Api/Persistence/ContextBook.cs
@@ -0,0 +1,19 @@
+﻿using BookStore.Books.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Books.Api.Persistence
+{
+    public class ContextBook :DbContext
+    {
+        public ContextBook(DbContextOptions<ContextBook> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
