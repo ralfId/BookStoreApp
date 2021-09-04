207029b7db98dd535ab4a553e5da50908948b67c
rafaelId <ralf_raid@outlook.es>
2021-09-04 11:48:17 -0600

    codefirst migration to sSqlServer

---
+++ BookStore.Books.Api/Migrations/20210904170917_InitialSQLServerMigration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStore.Books.Api.Migrations
+{
+    public partial class InitialSQLServerMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    BookId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PublicationDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    AuthoBook = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.BookId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
--- BookStore.Books.Api/Startup.cs
+++ BookStore.Books.Api/Startup.cs
@@ -1,6 +1,8 @@
+using BookStore.Books.Api.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -31,6 +33,8 @@ namespace BookStore.Books.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookStore.Books.Api", Version = "v1" });
             });
+
+            services.AddDbContext<ContextBook>(config => { config.UseSqlServer(Configuration.GetConnectionString("ConnectionDB")); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
