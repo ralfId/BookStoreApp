fa244d00de02687871af161b0245d3cb6d91318b
rafaelId <ralf_raid@outlook.es>
2021-09-06 14:02:50 -0600

    remote services for books

---
+++ BookStore.Shop.Api/RemoteModels/BookRM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Shop.Api.RemoteModels
+{
+    public class BookRM
+    {
+        public Guid BookId { get; set; }
+        public string Title { get; set; }
+        public DateTime? PublicationDate { get; set; }
+        public Guid? AuthoBook { get; set; }
+    }
+}
---
+++ BookStore.Shop.Api/RemoteServices/IServices/IBookServices.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Shop.Api.RemoteModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Shop.Api.RemoteServices.IServices
+{
+    public interface IBookServices
+    {
+        Task<(bool result, BookRM book, string error)> GetBook(Guid BookId);
+    }
+}
