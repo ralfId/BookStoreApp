647385362b04956a933f88121d5e526c32fcd58e
rafaelId <ralf_raid@outlook.es>
2021-09-06 11:44:33 -0600

    Add models Dto

---
+++ BookStore.Shop.Api/Application/ModelsDto/ShoppingCartDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Shop.Api.Application.ModelsDto
+{
+    public class ShoppingCartDto
+    {
+        public int ShoppingId { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public List<ShoppingDetailDto> ProductList { get; set; }
+    }
+}
---
+++ BookStore.Shop.Api/Application/ModelsDto/ShoppingDetailDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Shop.Api.Application.ModelsDto
+{
+    public class ShoppingDetailDto
+    {
+        public Guid? BookId { get; set; }
+        public string BookTitle { get; set; }
+        public string Author { get; set; }
+        public DateTime? Creationdate { get; set; }
+    }
+}
