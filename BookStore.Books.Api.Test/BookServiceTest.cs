72b03c487fe4b5912de2d4eede7cc3ec17586904
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-11-20 11:27:22 -0600

    create mocks and unit test methods

--- BookStore.Books.Api.Test/BookServiceTest.cs
+++ BookStore.Books.Api.Test/BookServiceTest.cs
@@ -3,11 +3,20 @@
 namespace BookStore.Books.Api.Test
 {
     public class BookServiceTest
-    {
+    { 
         [Fact]
         public void GetBooks()
         {
+            /**
+             * Que me todo dentro del microservicio se encarga de realizar la consulta 
+             * de libros de la base de datos?
+             */
 
+            /**
+             * 1. Emular a la instancia de Entity Framework Core ContextBook
+             * para emular las acciones y eventos de un objeto en un ambiente unit test
+             * utilizando objetos de tipo mock
+             */
         }
     }
 }
