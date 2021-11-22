70329008a8dc8aea8dee6a63599de12ef9acb4c0
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-11-22 15:21:42 -0600

    creacion de metodosde unit testing

---
+++ BookStore.Books.Api.Test/Helpers/MappingTest.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BookStore.Books.Api.Application.ModelsDto;
+using BookStore.Books.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Books.Api.Test.Helpers
+{
+    public class MappingTest : Profile
+    {
+        public MappingTest()
+        {
+            CreateMap<Book, BookDto>();
+        }
+    }
+}
