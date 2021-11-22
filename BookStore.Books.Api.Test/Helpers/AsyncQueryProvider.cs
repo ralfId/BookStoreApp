29a82345b30782c751e264d3a6ab7273a3203ed6
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-11-22 15:40:36 -0600

    generic class to query objects by id or create/post/put items on unit test

---
+++ BookStore.Books.Api.Test/Helpers/AsyncQueryProvider.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BookStore.Books.Api.Test.Helpers
+{
+    public class AsyncQueryProvider<TEntity> : IAsyncQueryProvider
+    {
+        private readonly IQueryProvider _queryProvider;
+
+        public AsyncQueryProvider(IQueryProvider queryProvider)
+        {
+            _queryProvider = queryProvider;
+        }
+
+        public IQueryable CreateQuery(Expression expression)
+        {
+            return new AsyncEnumerable<TEntity>(expression);
+        }
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            return new AsyncEnumerable<TElement>(expression);
+        }
+
+        public object Execute(Expression expression)
+        {
+            return _queryProvider.Execute(expression);
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            return _queryProvider.Execute<TResult>(expression);
+        }
+
+        public TResult ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken = default)
+        {
+            var resultType = typeof(TResult).GetGenericArguments()[0];
+            var execResult = typeof(IQueryProvider).GetMethod(name: nameof(IQueryProvider.Execute), genericParameterCount: 1, types: new[] { typeof(Expression) }).MakeGenericMethod(resultType).Invoke(this, new[] { expression });
+
+            return (TResult)typeof(Task).GetMethod(nameof(Task.FromResult))?
+                .MakeGenericMethod(resultType).Invoke(null, new[] { execResult });
+        }
+    }
+}
